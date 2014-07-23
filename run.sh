ad73bd0c2719f66826b85332808df9fb54287f7a
Daisuke Maki <lestrrat+github@gmail.com>
2014-07-23 16:47:20 +0900

    Update step to use https URL with github token

--- run.sh
+++ run.sh
@@ -7,8 +7,14 @@ if [ ! -n "$RELEASE_TAG" ]; then
     exit 0
 fi
 
+if [ ! -n "$GITHUB_TOKEN" ]; then
+    echo "no release github token"
+    exit 0
+fi
+
 git clone git@github.com:peco/homebrew-peco.git
 cd homebrew-peco             
 go run make.go peco $RELEASE_TAG
 git commit -m "Update version" peco.rb
-git push origin master   
+REMOTE="https://$GITHUB_TOKEN@github.com/peco/homebrew-peco.git"
+git push $REMOTE master   
