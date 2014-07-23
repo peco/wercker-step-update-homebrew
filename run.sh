df3ce6eeca73e8c5f2a7ce032f9594ecaaca80cd
Daisuke Maki <lestrrat+github@gmail.com>
2014-07-23 15:08:19 +0900

    initial commit

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -n "$RELEASE_TAG" ]; then
+    echo "no release tag"
+    exit 1
+fi
+
+git clone git@github.com:peco/homebrew-peco.git
+cd homebrew-peco             
+go run make.go peco $RELEASE_TAG
+git commit -m "Update version" peco.rb
+git push origin master   
